1a9c8a2496b7b7dadc50000f49ce611ae3c94864
Zenedith <github@zenedith.mailgun.org>
2016-01-01 00:25:42 +0100

    Added new enum value for brand

--- test/model/brandTest.js
+++ test/model/brandTest.js
@@ -4,11 +4,19 @@ var Brand = require('../../lib/model/brand').Brand;
 
 describe('brand enum test', function () {
 
-  it('should get brand', function (done) {
+  it('should get brand for HAIRDRESSING', function (done) {
     var brand = Brand.getBrand('HAIRDRESSING');
 
     should.exist(brand);
     brand.should.be.equal(Brand.HAIRDRESSING);
+    done();
+  });
+
+  it('should get brand for PRIVATE_MEDIC_DENTAL', function (done) {
+    var brand = Brand.getBrand('PRIVATE_MEDIC_DENTAL');
+
+    should.exist(brand);
+    brand.should.be.equal(Brand.PRIVATE_MEDIC_DENTAL);
     done();
   });
 
